50d749cdf81d3c2a504795f796259245d58da9b4
DESKTOP-NFGF3PG\zxa01 <zxa011023>
2018-12-04 20:59:39 +0800

    remove no use file

--- src/app/pages/extra-components/extra-components-routing.module.ts
+++ src/app/pages/extra-components/extra-components-routing.module.ts
@@ -2,32 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ExtraComponentsComponent } from './extra-components.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { NebularFormInputsComponent } from './form-inputs/nebular-form-inputs.component';
-import { InfiniteListComponent } from './infinite-list/infinite-list.component';
-import { ListComponent } from './list/list.component';
 
 const routes: Routes = [{
   path: '',
   component: ExtraComponentsComponent,
-  children: [
-    {
-      path: 'list',
-      component: ListComponent,
-    },
-    {
-      path: 'infinite-list',
-      component: InfiniteListComponent,
-    },
-    {
-      path: 'form-inputs',
-      component: NebularFormInputsComponent,
-    },
-    {
-      path: 'progress-bar',
-      component: ProgressBarComponent,
-    },
-  ],
 }];
 
 @NgModule({
--- src/app/pages/extra-components/extra-components.module.ts
+++ src/app/pages/extra-components/extra-components.module.ts
@@ -2,37 +2,15 @@ import { NgModule } from '@angular/core';
 
 import { TreeModule } from 'angular-tree-component';
 import { ToasterModule } from 'angular2-toaster';
-
 import { ThemeModule } from '../../@theme/theme.module';
 import { ExtraComponentsRoutingModule } from './extra-components-routing.module';
-
 import { ExtraComponentsComponent } from './extra-components.component';
-import {
-  InteractiveProgressBarComponent,
-} from './progress-bar/interactive-progress-bar/interactive-progress-bar.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { ListComponent } from './list/list.component';
-import { InfiniteListComponent } from './infinite-list/infinite-list.component';
-import { NewsPostComponent } from './infinite-list/news-post/news-post.component';
-import { NewsPostPlaceholderComponent } from './infinite-list/news-post-placeholder/news-post-placeholder.component';
-import { NebularFormInputsComponent } from './form-inputs/nebular-form-inputs.component';
-import { NebularSelectComponent } from './form-inputs/nebular-select/nebular-select.component';
 
 // service
 import { NewsService } from './services/news.service';
 
 const COMPONENTS = [
   ExtraComponentsComponent,
-  ProgressBarComponent,
-  InteractiveProgressBarComponent,
-  ListComponent,
-  InfiniteListComponent,
-  NewsPostComponent,
-  NewsPostPlaceholderComponent,
-  NebularFormInputsComponent,
-  NebularSelectComponent,
-  CalendarKitFullCalendarShowcaseComponent,
-  CalendarKitMonthCellComponent,
 ];
 
 const SERVICES = [
